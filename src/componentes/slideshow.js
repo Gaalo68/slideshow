3b10203fc5d889a3010b59e556c114dad50b450c
luisguillermomolero <gustavoangel745@gmail.com>
2021-11-03 15:15:09 -0500

    firts commit

---
+++ src/componentes/slideshow.js
@@ -0,0 +1,21 @@
+import React from "react";
+import img1 from './../img/1.jpg'
+import img2 from './../img/2.jpg'
+import img3 from './../img/3.jpg'
+import img4 from './../img/4.jpg'
+import {ReactComponente as FlechaIzquierda} from './../img/right-thin';
+
+const Slideshow = () => {
+    return (
+        <div>
+            <div>
+                <a href="https://github.com/Gaalo68">
+                <img src="" alt=""/>
+                </a>
+            </div>
+            <div>Controles</div>
+        </div>
+    );
+}
+
+export default Slideshow;
